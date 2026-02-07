176b88a16d3926ea0b617274a8565b7dba7becb1
Kgghoul <kgghoul@users.noreply.github.com>
2026-02-07 23:39:23 +0500

    Add environment interface and finalize API integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
   production: false,
   apiUrl: 'http://91.229.11.191:8080',
   playerId: '101'
